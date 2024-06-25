ba9ddfdc0bedb28fc801062a1306e798edc8ea38
takaharayota531 <takahara.yota@gmail.com>
2024-06-25 10:16:54 +0900

    0625-1

--- Assets/MyResearch/Scripts/ProgressController.cs
+++ Assets/MyResearch/Scripts/ProgressController.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class ProgressController : MonoBehaviour
 {
     private const int EXP_COUNT = 5;
-    private const
+    private const int PLACE_COUNT = 4;
     [SerializeField] private int remainingCount;
     [SerializeField] private TextMeshProUGUI counterText;
     // Start is called before the first frame update
