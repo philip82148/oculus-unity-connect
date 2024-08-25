f534789d51428cfc75343572c54fcca7966e6299
takaharayota531 <takahara.yota@gmail.com>
2024-08-25 21:06:59 +0900

    commit

--- Assets/MyResearch/Scripts/CarDriving/CarDrivingMainController.cs
+++ Assets/MyResearch/Scripts/CarDriving/CarDrivingMainController.cs
@@ -8,6 +8,14 @@ public class CarDrivingMainController : MonoBehaviour
     [SerializeField] private InstructionController instructionController;
     [SerializeField]
     private TextMeshProUGUI successDisplayText;
+    [SerializeField] private GameObject countDownPanel;
+    [SerializeField] private TextMeshProUGUI countDownText;
+    [SerializeField] private TextMeshProUGUI countText;
+
+    private float countTime = 5.0f;
+    private float gameTime = 20f;
+    private bool isCountDown = false;
+    private bool isGame = false;
 
 
     void Start()
@@ -16,57 +24,36 @@ public class CarDrivingMainController : MonoBehaviour
     }
 
     // Update is called once per frame
+    // Update is called once per frame
     void Update()
     {
-        Debug.Log(InstructionController.GetTmpIsInInstruction());
-        if (InstructionController.GetTmpIsInInstruction())
+        if (OVRInput.GetDown(OVRInput.Button.Four))
         {
-            if (Input.GetKeyDown(KeyCode.Alpha1))
+            isCountDown = true;
+            countDownPanel.SetActive(true);
+        }
+        if (isCountDown)
+        {
+            countTime -= Time.deltaTime;
+            countDownText.text = "count:" + countTime.ToString("F1");
+        }
+        if (countTime <= 0)
+        {
+            isCountDown = false;
+            countDownPanel.SetActive(false);
+            isGame = true;
+        }
+        if (isGame)
+        {
+            gameTime -= Time.deltaTime;
+            countText.text = "" + gameTime.ToString("F1");
+            if (gameTime <= 0)
             {
-                if (InstructionController.GetTmpInstruction() == InstructionController.instructions[0])
-                {
-                    ProcessCorrectAnswer();
-                }
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha2))
-            {
-                if (InstructionController.GetTmpInstruction() == InstructionController.instructions[1])
-                {
-                    ProcessCorrectAnswer();
-                }
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha3))
-            {
-                if (InstructionController.GetTmpInstruction() == InstructionController.instructions[2])
-                {
-                    ProcessCorrectAnswer();
-                }
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha4))
-            {
-                if (InstructionController.GetTmpInstruction() == InstructionController.instructions[3])
-                {
-                    ProcessCorrectAnswer();
-                }
-            }
-            else if (Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-                if (InstructionController.GetTmpInstruction() == InstructionController.instructions[4])
-                {
-                    ProcessCorrectAnswer();
-                }
-            }
-            else if (Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                if (InstructionController.GetTmpInstruction() == InstructionController.instructions[5])
-                {
-                    ProcessCorrectAnswer();
-                }
+                countText.text = "game finished";
+                isGame = false;
             }
         }
     }
-
-
 
 
     private void ProcessCorrectAnswer()
